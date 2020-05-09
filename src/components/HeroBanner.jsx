293aee958ccc2e9ccd0626ef74f6bcfe66b12e4f
sagar <sagar_sys@live.com>
2020-05-09 15:58:49 +0400

    - Update hero banner content

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import FullHeightSection from './FullHeightSection'
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+})
 
 export default function HeroBanner() {
-    return <Typography variant="h1">SAGARSYS</Typography>
+    const classes = useStyles()
+    return (
+        <FullHeightSection className={classes.root} id="home">
+            <Typography variant="h1">SAGARSYS</Typography>
+            <Typography variant="h6">
+                Passionate web developer striving to make the web a better place
+                ;)
+            </Typography>
+        </FullHeightSection>
+    )
 }
