1776da3f5ab5cf5bd6dd3fda28fd6b98c1db0269
sagar <sagar_sys@live.com>
2020-05-09 13:01:11 +0400

    - NavBar comp => mobile & desktop version

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import List from '@material-ui/core/List'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItem from '@material-ui/core/ListItem'
+import useResizer from '../hooks/useResizer'
+import { makeStyles } from '@material-ui/core/styles'
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />
+}
+
+const useStyles = makeStyles({
+    desktop: {
+        display: 'flex',
+    },
+    mobile: {
+        display: 'block',
+        width: '250px',
+    },
+})
+
+export default function NavBar({ onClick }) {
+    const isMobile = useResizer()
+    const classes = useStyles()
+    return (
+        <List
+            className={isMobile ? classes.mobile : classes.desktop}
+            component="nav"
+            aria-label="main site navigation"
+        >
+            {['portfolio', 'projects', 'about'].map((navItem) => (
+                <ListItemLink
+                    key={navItem}
+                    href={`#${navItem}`}
+                    onClick={onClick}
+                >
+                    <ListItemText primary={navItem.toUpperCase()} />
+                </ListItemLink>
+            ))}
+        </List>
+    )
+}
