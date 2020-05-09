e7965168d191d2a580240420cb6056111b35de2e
sagar <sagar_sys@live.com>
2020-05-09 11:21:27 +0400

    - use material ui theme in app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import './App.scss'
-import Button from '@material-ui/core/Button'
-import { ThemeProvider } from '@material-ui/styles'
-import theme from './styles/theme'
+import { themeOptions } from './styles/materialUiTheme'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import {
+    createMuiTheme,
+    responsiveFontSizes,
+    ThemeProvider,
+} from '@material-ui/core/styles'
+import Routes from './components/Routes'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function App() {
+export default function App() {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    let theme = useMemo(
+        () => createMuiTheme(themeOptions({ prefersDarkMode })),
+        [prefersDarkMode]
+    )
+    theme = responsiveFontSizes(theme)
     return (
         <ThemeProvider theme={theme}>
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-            <Button variant="contained" color="secondary">
-                Hello World
-            </Button>
+            <CssBaseline />
+            <Routes />
         </ThemeProvider>
     )
 }
-
-export default App
