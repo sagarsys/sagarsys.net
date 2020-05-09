a36af614753eef11e4b00f279292fe5db24ed42b
sagar <sagar_sys@live.com>
2020-05-09 11:18:24 +0400

    - use resizer hook to detect devices

---
+++ src/hooks/useResizer.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import { useTheme } from '@material-ui/styles'
+
+export default function useResizer() {
+    const theme = useTheme()
+    const matches = useMediaQuery(theme.breakpoints.down('xs'))
+    const [isMobile, setIsMobile] = useState(matches)
+
+    useEffect(() => {
+        function handleSizeChange() {
+            return setIsMobile(matches)
+        }
+
+        window.addEventListener('resize', handleSizeChange)
+        return () => {
+            window.removeEventListener('resize', handleSizeChange)
+        }
+    }, [matches])
+
+    return isMobile
+}
