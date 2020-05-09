9d32540c4399316ace3d8d3a87443b06a1fdef40
sagar <sagar_sys@live.com>
2020-05-09 11:23:35 +0400

    - hero banner

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function HeroBanner() {
+    return <Typography variant="h1">SAGARSYS</Typography>
+}
