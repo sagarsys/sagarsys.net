c39bc6da81c71af411cdaa95c3909c0d4da8fd1f
sagar <sagar_sys@live.com>
2020-05-09 15:57:52 +0400

    - Routes => Main + Connect everything

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import HeroBanner from './HeroBanner'
 import AppBar from './AppBar'
+import Portfolio from './Portfolio'
+import Projects from './Projects'
+import About from './About'
+import Footer from './Footer'
 
-function Routes() {
+function Main() {
     return (
         <Container maxWidth={false} disableGutters={true}>
             <AppBar />
             <HeroBanner />
+            <Portfolio />
+            <Projects />
+            <About />
+            <Footer />
         </Container>
     )
 }
 
-export default Routes
+export default Main
