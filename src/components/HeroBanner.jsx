a8ebd02a8b545e4b0930da7412eb1d995fc8cb16
sagar <sagar_sys@live.com>
2020-05-09 16:06:47 +0400

    - Hero banner => add styles for mobile

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import FullHeightSection from './FullHeightSection'
+import Container from '@material-ui/core/Container'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
+        height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
+        paddingLeft: theme.spacing(3),
+        paddingRight: theme.spacing(3),
+        textAlign: 'center',
     },
-})
+}))
 
 export default function HeroBanner() {
     const classes = useStyles()
     return (
-        <FullHeightSection className={classes.root} id="home">
-            <Typography variant="h1">SAGARSYS</Typography>
-            <Typography variant="h6">
-                Passionate web developer striving to make the web a better place
-                ;)
-            </Typography>
+        <FullHeightSection id="home">
+            <Container className={classes.root} maxWidth={false}>
+                <Typography variant="h1">SAGARSYS</Typography>
+                <Typography variant="h6">
+                    Passionate web developer striving to make the web a better
+                    place ;)
+                </Typography>
+            </Container>
         </FullHeightSection>
     )
 }
