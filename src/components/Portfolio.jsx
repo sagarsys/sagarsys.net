f4747080a4f8d231e6402722f99590547b1aed41
sagar <sagar_sys@live.com>
2020-05-10 13:09:31 +0400

    - Portfolio: Hook it up xD =? UseEffect to fetch mock data + useState for loading and error

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import FullHeightSection from './FullHeightSection'
+import PortfolioList from './PortfolioList'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+    error: {
+        padding: theme.spacing(6, 0, 8),
+    },
+}))
 
 export default function Portfolio() {
+    const classes = useStyles()
+    const [portfolio, setPortfolio] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+    useEffect(() => {
+        async function fetchData() {
+            setError(null)
+            setIsLoading(true)
+            try {
+                const response = await fetch('/data/mock/portfolio.json')
+                return await response.json()
+            } catch (e) {
+                console.error(`Fetch portfolio failed: \n${e}`)
+                setError(e)
+                return []
+            }
+        }
+        fetchData()
+            .then(setPortfolio)
+            .then(() => setIsLoading(false))
+    }, [])
+
     return (
         <FullHeightSection id="portfolio">
             <Container fixed>
