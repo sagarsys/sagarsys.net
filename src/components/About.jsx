dea5002dc96542ad47d5ca54811a5c9f778b9f7f
sagar <sagar_sys@live.com>
2020-05-13 22:16:46 +0400

    - Add contact to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import FullHeightSection from './FullHeightSection'
+import { Contact } from './Contact'
 
 export default function About() {
     return (
         <FullHeightSection id="about">
             <Container fixed>
                 <Typography variant="h2">About</Typography>
+                <Contact />
             </Container>
         </FullHeightSection>
     )
