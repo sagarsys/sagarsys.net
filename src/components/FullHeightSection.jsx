b29099fd2035b83985ed76570022e7c8816e7f9f
sagar <sagar_sys@live.com>
2020-05-30 02:31:13 +0400

    MUI Theme: converted custom variable to number + refactor accordingly

--- src/components/FullHeightSection.jsx
+++ src/components/FullHeightSection.jsx
@@ -5,7 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles((theme) => ({
     root: {
         height: 'auto',
-        minHeight: `calc(100vh - ${theme.navBarHeight})`,
+        minHeight: `calc(100vh - ${theme.navBarHeight}px)`,
     },
 }))
 
