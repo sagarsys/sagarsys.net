34de5d604ff9823ba031627f6df895fd8297c60b
sagar <sagar_sys@live.com>
2020-05-30 00:39:00 +0400

    - Connect scroll top to main comp + add anchor toolbar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,16 +6,30 @@ import Portfolio from './Portfolio'
 import Projects from './Projects'
 import About from './About'
 import Footer from './Footer'
+import ScrollTop from './ScrollTop'
+import Fab from '@material-ui/core/Fab'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+import Toolbar from '@material-ui/core/Toolbar'
 
-function Main() {
+function Main(props) {
     return (
         <Container maxWidth={false} disableGutters={true}>
+            <Toolbar id="back-to-top-anchor" />
             <AppBar />
             <HeroBanner />
             <Portfolio />
             <Projects />
             <About />
             <Footer />
+            <ScrollTop {...props}>
+                <Fab
+                    color="secondary"
+                    size="small"
+                    aria-label="scroll back to top"
+                >
+                    <KeyboardArrowUpIcon />
+                </Fab>
+            </ScrollTop>
         </Container>
     )
 }
