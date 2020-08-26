72c34189f36c13b0195f27a84446b80286f72aa5
sagar <sagar_sys@live.com>
2020-08-26 23:55:45 +0400

    - update career timeline implementation

--- src/components/CareerTimelineItem.jsx
+++ src/components/CareerTimelineItem.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import TimelineItem from '@material-ui/lab/TimelineItem'
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator'
+import TimelineConnector from '@material-ui/lab/TimelineConnector'
+import TimelineContent from '@material-ui/lab/TimelineContent'
+import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent'
+import TimelineDot from '@material-ui/lab/TimelineDot'
+import Typography from '@material-ui/core/Typography'
+import WorkIcon from '@material-ui/icons/Work'
+import CareerTimelineItemContent from './CareerTimelineItemContent'
+import makeStyles from '@material-ui/core/styles/makeStyles'
 
-export default function CareerTimelineItem({ event }) {
+const useStyles = makeStyles((theme) => ({
+    connector: {
+        backgroundColor: theme.palette.secondary.main,
+    },
+}))
+
+export default function CareerTimelineItem({ event, isLast }) {
+    const classes = useStyles()
     return (
-        <p>
-            <span>{event.startDate}</span>
-            <span>{event.endDate}</span>
-            <span>{event.title}</span>
-            <span>{event.description}</span>
-        </p>
+        <TimelineItem>
+            <TimelineOppositeContent>
+                <Typography variant="h6" color="secondary">
+                    {event.startDate} - {event.endDate}
+                </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <TimelineDot color="secondary">
+                    <WorkIcon />
+                </TimelineDot>
+                {!isLast && <TimelineConnector className={classes.connector} />}
+            </TimelineSeparator>
+            <TimelineContent>
+                <CareerTimelineItemContent event={event} />
+            </TimelineContent>
+        </TimelineItem>
     )
 }
