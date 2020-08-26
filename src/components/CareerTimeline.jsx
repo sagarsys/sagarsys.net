2558f4702a51385d8550229c5ad0a9ad27e96863
sagar <sagar_sys@live.com>
2020-08-26 23:56:20 +0400

    - update career timeline

--- src/components/CareerTimeline.jsx
+++ src/components/CareerTimeline.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CareerTimelineItem from './CareerTimelineItem'
+import Timeline from '@material-ui/lab/Timeline'
+import useResizer from '../hooks/useResizer'
 
 export default function CareerTimeline({ events }) {
+    const isMobile = useResizer()
     return (
-        <pre>
+        <Timeline align={isMobile ? 'left' : 'alternate'}>
             {events.map((e, i) => (
-                <CareerTimelineItem event={e} key={i} />
+                <CareerTimelineItem
+                    event={e}
+                    key={i}
+                    isLast={i === events.length - 1}
+                />
             ))}
-        </pre>
+        </Timeline>
     )
 }
