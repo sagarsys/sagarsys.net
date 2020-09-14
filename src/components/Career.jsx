a5b3902b8cf5a175301919148cf521395b822a00
sagar <sagar_sys@live.com>
2020-09-14 16:07:18 +0400

    - career: use mobile vs desktop versions + relocate contact info

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import useFetchData from '../hooks/useFetchData'
-import FullHeightSection from './FullHeightSection'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
@@ -8,9 +7,11 @@ import ErrorMessage from './ErrorMessage'
 import CareerTimeline from './CareerTimeline'
 import { Skeleton } from '@material-ui/lab'
 
-const useStyles = makeStyles({
-    root: {},
-})
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(4, 0),
+    },
+}))
 
 export default function Career() {
     const classes = useStyles()
@@ -20,15 +21,11 @@ export default function Career() {
         'career section data'
     )
     return (
-        <FullHeightSection id="career" className={classes.root}>
-            <Container fixed>
-                <Typography variant="h3">Career</Typography>
-                {data && <CareerTimeline events={data} />}
-                {isLoading && (
-                    <Skeleton variant="rect" width="100%" height={500} />
-                )}
-                {error && <ErrorMessage error={error} />}
-            </Container>
-        </FullHeightSection>
+        <Container id="career" className={classes.root}>
+            <Typography variant="h3">Career</Typography>
+            {data && <CareerTimeline events={data} />}
+            {isLoading && <Skeleton variant="rect" width="100%" height={500} />}
+            {error && <ErrorMessage error={error} />}
+        </Container>
     )
 }
--- src/components/CareerTimelineItem.jsx
+++ src/components/CareerTimelineItem.jsx
@@ -1,39 +1,13 @@
 import React from 'react'
-import TimelineItem from '@material-ui/lab/TimelineItem'
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator'
-import TimelineConnector from '@material-ui/lab/TimelineConnector'
-import TimelineContent from '@material-ui/lab/TimelineContent'
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent'
-import TimelineDot from '@material-ui/lab/TimelineDot'
-import Typography from '@material-ui/core/Typography'
-import WorkIcon from '@material-ui/icons/Work'
-import CareerTimelineItemContent from './CareerTimelineItemContent'
-import makeStyles from '@material-ui/core/styles/makeStyles'
-
-const useStyles = makeStyles((theme) => ({
-    connector: {
-        backgroundColor: theme.palette.secondary.main,
-    },
-}))
+import useResizer from '../hooks/useResizer'
+import CareerTimelineItemDesktop from './CareerTimelineItemDesktop'
+import CareerTimelineItemMobile from './CareerTimelineItemMobile'
 
 export default function CareerTimelineItem({ event, isLast }) {
-    const classes = useStyles()
-    return (
-        <TimelineItem>
-            <TimelineOppositeContent>
-                <Typography variant="h6" color="secondary">
-                    {event.startDate} - {event.endDate}
-                </Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-                <TimelineDot color="secondary">
-                    <WorkIcon />
-                </TimelineDot>
-                {!isLast && <TimelineConnector className={classes.connector} />}
-            </TimelineSeparator>
-            <TimelineContent>
-                <CareerTimelineItemContent event={event} />
-            </TimelineContent>
-        </TimelineItem>
+    const isMobile = useResizer()
+    return isMobile ? (
+        <CareerTimelineItemMobile event={event} isLast={isLast} />
+    ) : (
+        <CareerTimelineItemDesktop event={event} isLast={isLast} />
     )
 }
